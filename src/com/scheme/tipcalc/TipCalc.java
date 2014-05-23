dc8ffca07255c2aef453c3ac46729c15e8436600
Kingclove <klouse@outlook.com>
2014-05-23 13:56:47 -0500

    Removed unused imports

--- src/com/scheme/tipcalc/TipCalc.java
+++ src/com/scheme/tipcalc/TipCalc.java
@@ -6,7 +6,6 @@ import android.os.SystemClock;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
-import android.view.TextureView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
